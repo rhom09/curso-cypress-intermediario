b03db9aa2a929855d7e7a10e58ceafd6ebd3542a
rhom09 <rhom0909@gmail.com>
2021-01-04 23:44:21 -0300

    Create tests API, GUI and CLI using Cypress

---
+++ cypress/integration/gui/logout.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Logout', () => {
+    beforeEach(() => cy.login())
+
+    it('successfully', () => {
+        cy.logout()
+
+        cy.url().should('be.equal', `${Cypress.config('baseUrl')}users/sign_in`)
+    });
+});
---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+
+Cypress.Commands.add('login', () => {
+    cy.visit('users/sign_in');
+
+    // Para proteção de dados sensiveis é usado o Cypress env que fica visivel na nossa maquina
+    cy.get('[data-qa-selector="login_field"]').type(Cypress.env('user_name'));
+    cy.get('[data-qa-selector="password_field"]').type(Cypress.env('user_password'));
+    cy.get('[data-qa-selector="sign_in_button"]').click();
+})
+
+Cypress.Commands.add('logout', () => {
+    cy.get('.qa-user-avatar').click();
+    cy.contains('Sign out').click();
+})
+
+// Recebe um parametro(project) que é a const criada na spec
+Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('projects/new')
+
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
+    cy.wait(5000);
+})
+
+Cypress.Commands.add('gui_createIssue', issue => {
+    cy.visit(`${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
+
+    cy.get('.qa-issuable-form-title').type(issue.title)
+    cy.get('.qa-issuable-form-description').type(issue.description)
+    cy.contains('Submit issue').click()
+    cy.wait(5000)
+})
+
+Cypress.Commands.add('gui_setLabelOnIssue', label => {
+    cy.get('.qa-edit-link-labels').click()
+    cy.contains(label.name).click()
+    cy.get('body').click()
+  })
+
+  Cypress.Commands.add('gui_setMilestoneOnIssue', milestone => {
+    cy.get('.block.milestone .edit-link').click()
+    cy.contains(milestone.title).click()
+  })
+  
+  
